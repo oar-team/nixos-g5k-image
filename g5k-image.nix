56ad1a1a3dd380d6d5ab5e0edbfbdc8441f60a44
Olivier Richard <olivier.richard@imag.fr>
2020-11-06 09:59:44 +0100

    add /dev/disk/by-label/ support

--- g5k-image.nix
+++ g5k-image.nix
@@ -11,6 +11,22 @@
     <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
   ];
 
+  # Set and select root system by label (nixos) 
+  boot.initrd.extraUtilsCommands = ''
+    copy_bin_and_libs ${pkgs.e2fsprogs}/bin/e2label
+  '';
+
+  boot.initrd.postDeviceCommands = ''
+    for o in $(cat /proc/cmdline); do
+    case $o in
+            root=*)
+                set -- $(IFS==; echo $o)
+                e2label $2 nixos
+                ;;
+        esac
+    done
+  '';
+  
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
@@ -20,7 +36,7 @@
   boot.kernelModules = [ "kvm-intel" ];
   
   fileSystems."/" = {
-    device = "/dev/disk/by-label/System";
+    device = "/dev/disk/by-label/nixos";
     autoResize = true;
     fsType = "ext4";
   };
@@ -29,26 +45,48 @@
 
   nix.maxJobs = lib.mkDefault 32;
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
-  
+
   system.build.g5k-image = import "${toString modulesPath}/../lib/make-system-tarball.nix" {
     stdenv = pkgs.stdenv;
     closureInfo = pkgs.closureInfo;
     pixz = pkgs.pixz;
+    extraCommands = "mkdir -p etc/ssh root tmp var/log";
     storeContents = [
       { object = config.system.build.toplevel; symlink = "/run/current-system"; }
     ];
 
-    contents =
-      [ { source = config.system.build.initialRamdisk + "/" + config.system.boot.loader.initrdFile;
-      target = "/boot/" + config.system.boot.loader.initrdFile;
-    }
-      #{ source = versionFile;
-      #  target = "/nixos-version.txt";
-      #}
+    contents = [
+      { source = config.system.build.initialRamdisk + "/" + config.system.boot.loader.initrdFile;
+        target = "/boot/" + config.system.boot.loader.initrdFile;
+      }
+      { source = config.boot.kernelPackages.kernel + "/" + config.system.boot.loader.kernelFile;
+        target = "/boot/" + config.system.boot.loader.kernelFile;
+      }
+      { source = "${builtins.unsafeDiscardStringContext config.system.build.toplevel}/init";
+        target = "/boot/init";
+      }
     ];
 
   };
 
+  system.build.g5k-image-info = pkgs.writeText "g5k-imag-info.json" (builtins.toJSON {
+    kernel=config.boot.kernelPackages.kernel + "/" + config.system.boot.loader.kernelFile;
+    initrd=config.system.build.initialRamdisk + "/" + config.system.boot.loader.initrdFile;
+    init="${builtins.unsafeDiscardStringContext config.system.build.toplevel}/init";
+    image = config.system.build.g5k-image;
+  });
+    
+  # system.build.g5k-image-all = pkgs.stdenv.mkDerivation {
+  #     name = "g5k-image-all";
+  #     dontUnpack = true;
+  #     doCheck = false;
+
+  #     installPhase =''
+  #       mkdir -p $out
+  #       cp ${kexec_qemu_script} $out/bin/kexec-qemu
+  #       cp ${kexec_info} $out/kexec-info.json
+  #     '';
+  #   };
 
   boot.postBootCommands =
   ''
@@ -64,12 +102,8 @@
     touch /etc/NIXOS
     ${config.nix.package.out}/bin/nix-env -p /nix/var/nix/profiles/system --set /run/current-system
   '';
-
-
-
-
-
   
-  formatAttr = "g5k-image";
+    #formatAttr = "g5k-image";
+    formatAttr = "g5k-image-info";
   filename = "*.img";
 }
