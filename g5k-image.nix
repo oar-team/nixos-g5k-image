ec43f73c917cfd0b96bb15ce8ad45cb3f33da74f
Olivier Richard <olivier.richard@imag.fr>
2020-11-07 12:50:41 +0100

    add kadaploy description environment file generation

--- g5k-image.nix
+++ g5k-image.nix
@@ -1,15 +1,36 @@
 { config, lib, pkgs, modulesPath, ... }:
-{
+let
+  kaenv_name = if (builtins.getEnv ("KAENV_NAME") != "") then
+    builtins.getEnv ("KAENV_NAME")
+  else
+    "nixos-${pkgs.stdenv.hostPlatform.system}";
+
+  author = if (builtins.getEnv ("AUTHOR") != "") then
+    builtins.getEnv ("AUTHOR")
+  else
+    builtins.getEnv ("USER");
+
+  image_name = kaenv_name;
+
+  # image_file = if (builtins.getEnv("YOP") != "") then 
+  # builtins.getEnv("YOP")
+  # else "hello";
+
+  postinstall = if (builtins.getEnv ("POST_INSTALL") != "") then
+    builtins.getEnv ("POST_INSTALL")
+  else
+    "http://public.nancy.grid5000.fr/~orichard/postinstalls/g5k-nixos-postinstall-2020110500.tgz";
+in {
 
   imports =
-    
+
     [
-     # Profiles of this basic installation.
-     <nixpkgs/nixos/modules/profiles/all-hardware.nix>
-     <nixpkgs/nixos/modules/profiles/base.nix>
-     <nixpkgs/nixos/modules/profiles/installation-device.nix>
-    <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
-  ];
+      # Profiles of this basic installation.
+      <nixpkgs/nixos/modules/profiles/all-hardware.nix>
+      <nixpkgs/nixos/modules/profiles/base.nix>
+      <nixpkgs/nixos/modules/profiles/installation-device.nix>
+      <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
+    ];
 
   # Set and select root system by label (nixos) 
   boot.initrd.extraUtilsCommands = ''
@@ -27,15 +48,16 @@
         esac
     done
   '';
-  
+
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
   boot.loader.grub.device = "/dev/sda3";
-  
-  boot.initrd.availableKernelModules = [ "ahci" "ehci_pci" "megaraid_sas" "sd_mod" ];
+
+  boot.initrd.availableKernelModules =
+    [ "ahci" "ehci_pci" "megaraid_sas" "sd_mod" ];
   boot.kernelModules = [ "kvm-intel" ];
-  
+
   fileSystems."/" = {
     device = "/dev/disk/by-label/nixos";
     autoResize = true;
@@ -47,65 +69,110 @@
   nix.maxJobs = lib.mkDefault 32;
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
 
-  system.build.g5k-image = import "${toString modulesPath}/../lib/make-system-tarball.nix" {
-    stdenv = pkgs.stdenv;
-    closureInfo = pkgs.closureInfo;
-    pixz = pkgs.pixz;
-    extraCommands = "mkdir -p etc/ssh root tmp var/log";
-    storeContents = [
-      { object = config.system.build.toplevel; symlink = "/run/current-system"; }
-    ];
+  system.build.g5k-image =
+    import "${toString modulesPath}/../lib/make-system-tarball.nix" {
+      fileName = image_name;
+      stdenv = pkgs.stdenv;
+      closureInfo = pkgs.closureInfo;
+      pixz = pkgs.pixz;
+      extraCommands = "mkdir -p etc/ssh root tmp var/log";
+      storeContents = [{
+        object = config.system.build.toplevel;
+        symlink = "/run/current-system";
+      }];
 
-    contents = [
-      { source = config.system.build.initialRamdisk + "/" + config.system.boot.loader.initrdFile;
-        target = "/boot/" + config.system.boot.loader.initrdFile;
-      }
-      { source = config.boot.kernelPackages.kernel + "/" + config.system.boot.loader.kernelFile;
-        target = "/boot/" + config.system.boot.loader.kernelFile;
-      }
-      { source = "${builtins.unsafeDiscardStringContext config.system.build.toplevel}/init";
-        target = "/boot/init";
-      }
-    ];
+      contents = [
+        {
+          source = config.system.build.initialRamdisk + "/"
+            + config.system.boot.loader.initrdFile;
+          target = "/boot/" + config.system.boot.loader.initrdFile;
+        }
+        {
+          source = config.boot.kernelPackages.kernel + "/"
+            + config.system.boot.loader.kernelFile;
+          target = "/boot/" + config.system.boot.loader.kernelFile;
+        }
+        {
+          source = "${
+              builtins.unsafeDiscardStringContext config.system.build.toplevel
+            }/init";
+          target = "/boot/init";
+        }
+      ];
 
+    };
+
+  system.build.g5k-image-info = pkgs.writeText "g5k-image-info.json"
+    (builtins.toJSON {
+      kernel = config.boot.kernelPackages.kernel + "/"
+        + config.system.boot.loader.kernelFile;
+      initrd = config.system.build.initialRamdisk + "/"
+        + config.system.boot.loader.initrdFile;
+      init = "${
+          builtins.unsafeDiscardStringContext config.system.build.toplevel
+        }/init";
+      image = config.system.build.g5k-image;
+      kaenv = config.system.build.kadeploy_env_description;
+    });
+
+  system.build.g5k-image-all = pkgs.stdenv.mkDerivation {
+    name = "g5k-image-all";
+    dontUnpack = true;
+    doCheck = false;
+
+    installPhase = ''
+      mkdir $out
+      ln -s ${config.system.build.g5k-image-info} $out/g5k-image-info.json
+      ln -s ${config.system.build.kadeploy_env_description} $out/kaenv.yaml
+      ln -s ${config.system.build.g5k-image} $out/${image_name}.tar.xz
+    '';
   };
 
-  system.build.g5k-image-info = pkgs.writeText "g5k-imag-info.json" (builtins.toJSON {
-    kernel=config.boot.kernelPackages.kernel + "/" + config.system.boot.loader.kernelFile;
-    initrd=config.system.build.initialRamdisk + "/" + config.system.boot.loader.initrdFile;
-    init="${builtins.unsafeDiscardStringContext config.system.build.toplevel}/init";
-    image = config.system.build.g5k-image;
-  });
-    
-  # system.build.g5k-image-all = pkgs.stdenv.mkDerivation {
-  #     name = "g5k-image-all";
-  #     dontUnpack = true;
-  #     doCheck = false;
-
-  #     installPhase =''
-  #       mkdir -p $out
-  #       cp ${kexec_qemu_script} $out/bin/kexec-qemu
-  #       cp ${kexec_info} $out/kexec-info.json
-  #     '';
-  #   };
-
-  boot.postBootCommands =
-  ''
+  boot.postBootCommands = ''
     # After booting, register the contents of the Nix store on the
     # CD in the Nix database in the tmpfs.
     if [ -f /nix-path-registration ]; then
     ${config.nix.package.out}/bin/nix-store --load-db < /nix-path-registration &&
     rm /nix-path-registration
     fi
-    
+
     # nixos-rebuild also requires a "system" profile and an
     # /etc/NIXOS tag.
     touch /etc/NIXOS
     ${config.nix.package.out}/bin/nix-env -p /nix/var/nix/profiles/system --set /run/current-system
   '';
 
+  system.build.kadeploy_env_description = pkgs.writeTextFile {
+    name = "kaenv.yaml";
+    text = ''
+      name: ${kaenv_name}
+      version: 1
+      description: NixOS 
+      author: ${author}
+      visibility: shared
+      destructive: false
+      os: linux
+      image:
+        file: http://public.grenoble.grid5000.fr/~orichard/nixos-system-x86_64-linux-debug.tar.xz
+        kind: tar
+        compression: xz
+      postinstalls:
+      - archive: ${postinstall}
+        compression: gzip
+        script: g5k-postinstall --debug --net none 
+      boot:
+        kernel: /boot/bzImage 
+        initrd: /boot/initrd
+        kernel_params: init=boot/init console=tty0 console=ttyS0,115200 debugtrace 
+      filesystem: ext4
+      partition_type: 131
+      multipart: false
+    '';
+  };
+
   networking.hostName = "";
-    #formatAttr = "g5k-image";
-    formatAttr = "g5k-image-info";
+  formatAttr = "g5k-image-all";
+  #formatAttr = "g5k-image";
+  #formatAttr = "g5k-image-info";
   filename = "*.img";
 }
